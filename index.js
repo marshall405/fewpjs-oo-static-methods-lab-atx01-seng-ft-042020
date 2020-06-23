2f9e84c116926f8d9894a0f7937104f9dbc680a5
marshall405 <marshall.slemp@gmail.com>
2020-06-22 19:43:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z-'\s]+/g, '')
+  }
+
+  static titleize(str) {
+    const wordsNotToCap = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const words = str.split(" ").map((word, i) => {
+      if (wordsNotToCap.indexOf(word) < 0 || i == 0) {
+        return this.capitalize(word)
+      } else {
+        return word
+      }
+    })
+    return words.join(" ")
+  }
 }
